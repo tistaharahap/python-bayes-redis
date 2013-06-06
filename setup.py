1a85fe0733252188754859965e3be37f7e1bfe36
Batista Harahap <batista@bango29.com>
2013-06-07 01:47:11 +0700

    dependencies now figured

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 
-setup(
-    ext_modules = [Extension("BayesRedis", ["BayesRedis.c"])]
-)
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(ext_modules=[Extension("BayesRedis", ["BayesRedis/__init__.c"])],
+      name='bayesredis',
+      version='1.0.3',
+      description='A Simple Naive Bayes Classifier in Python',
+      long_description=readme(),
+      keywords='bayes naive classifier redis cython',
+      url='https://github.com/tistaharahap/python-bayes-redis',
+      author='Batista Harahap',
+      author_email='batista@bango29.com',
+      license='MIT',
+      packages=['BayesRedis'],
+      setup_requires=['redis>=2.7.0'],
+      zip_safe=False)
